670ef57ebd58cb01087177c6c0fa3507999a5477
Reid Scott Campbell <44685146+ReidCampbell@users.noreply.github.com>
2019-08-12 20:12:13 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -86,7 +86,7 @@ export default class Header extends Component {
                             </ul>
                             <button className="btn-download">
                                 <a
-                                    href="https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/ReidCampbell/Portfolio/blob/master/public/ReidScottCampbell%20Resume.pdf"
+                                    href="https://github.com/ReidCampbell/Portfolio/raw/master/public/ReidScottCampbell%20Resume.pdf"
                                     download
                                 >
                                     Download Resume
