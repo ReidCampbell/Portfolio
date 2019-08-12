a3923cbad98df7f407e6fc029658f046e570e353
ReidCampbell <reidscottcampbell@gmail.com>
2019-08-12 16:33:11 +0100

    Updates

---
+++ precache-manifest.6334b7113ac56a28f33b62794ad59789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "fff470f7f7e9886ac41b",
+    "url": "/portfolio/static/js/main.fff470f7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "fff470f7f7e9886ac41b",
+    "url": "/portfolio/static/css/main.aa80502b.chunk.css"
+  },
+  {
+    "revision": "a50717d7799cec4d0ad425ca21e6cb81",
+    "url": "/portfolio/index.html"
+  }
+];
