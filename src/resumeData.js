794fd602a280297ef1c5406062979d6e3058faa8
ReidCampbell <reidscottcampbell@gmail.com>
2019-08-12 16:00:33 +0100

    inital commit, changes to be made to about me

---
+++ src/resumeData.js
@@ -0,0 +1,128 @@
+let resumeData = {
+    imagebaseurl: "https://rbhatia46.github.io/",
+    name: "Reid Campbell",
+    role: "Full Stack Web Developer",
+    linkedinId: "ReidScottCampbell",
+    roleDescription:
+        " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    socialLinks: [
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/ReidScottCampbell/",
+            className: "fa fa-linkedin"
+        },
+        {
+            name: "github",
+            url: "http://github.com/ReidCampbell",
+            className: "fa fa-github"
+        }
+    ],
+    aboutme: "",
+    address: "London",
+    email: "reidscottcampbell@gmail.com",
+    education: [
+        {
+            UniversityName: "Le Wagon",
+            specialization: "Full Stack Web Development",
+            MonthOfPassing: "June",
+            YearOfPassing: "2019",
+            Achievements: "Some Achievements"
+        },
+        {
+            UniversityName: "University of Gloucestershire",
+            specialization: "Marketing, Advertising and Branding",
+            MonthOfPassing: "May",
+            YearOfPassing: "2017",
+            Achievements: "2.1"
+        }
+    ],
+    work: [
+        {
+            CompanyName: "OMD",
+            specialization: "Biddable Performance Executive",
+            MonthOfLeaving: "April",
+            YearOfLeaving: "2019",
+            Achievements: "Some Achievements"
+        },
+        {
+            CompanyName: "JCDecaux",
+            specialization: "Strategic Solutions Sales Coordinator",
+            MonthOfLeaving: "August",
+            YearOfLeaving: "2018",
+            Achievements: "Some Achievements"
+        }
+    ],
+    skillsDescription: "Technologies",
+    skills: [
+        {
+            skillname: "HTML5",
+            logo: "devicon-html5-plain-wordmark colored"
+        },
+        {
+            skillname: "CSS",
+            logo: "devicon-css3-plain-wordmark colored"
+        },
+        {
+            skillname: "Javascript",
+            logo: "devicon-javascript-plain colored"
+        },
+        {
+            skillname: "Ruby",
+            logo: "devicon-ruby-plain-wordmark colored"
+        },
+        {
+            skillname: "ReactJS",
+            logo: "devicon-react-original-wordmark colored"
+        },
+        {
+            skillname: "Rails",
+            logo: "devicon-rails-plain-wordmark colored"
+        },
+        {
+            skillname: "MySQL",
+            logo: "devicon-mysql-plain-wordmark colored"
+        },
+        {
+            skillname: "Adobe Creative Studio",
+            logo: "devicon-photoshop-line colored"
+        },
+        {
+            skillname: "Git",
+            logo: "devicon-git-plain-wordmark colored"
+        }
+    ],
+    portfolio: [
+        {
+            name: "brunchee.",
+            description: "Web App",
+            imgurl: "images/portfolio/brunchee.png"
+        }
+        // {
+        //     name: "project2",
+        //     description: "mobileapp",
+        //     imgurl: "images/portfolio/project.jpg"
+        // },
+        // {
+        //     name: "project3",
+        //     description: "mobileapp",
+        //     imgurl: "images/portfolio/project2.png"
+        // },
+        // {
+        //     name: "project4",
+        //     description: "mobileapp",
+        //     imgurl: "images/portfolio/phone.jpg"
+        // }
+    ]
+    // testimonials: [
+    //     {
+    //         description: "",
+    //         name: ""
+    //     },
+    //     {
+    //         description: "",
+    //         name: ""
+    //     }
+    // ]
+};
+
+export default resumeData;
