5686d5b58ad97ed21edcf744c22e6b8797759150
ReidCampbell <reidscottcampbell@gmail.com>
2020-10-22 11:48:50 +0100

    Add reiddit project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,64 +1,58 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 export default class Porfolio extends Component {
-    render() {
-        let resumeData = this.props.resumeData;
-        return (
-            <section id="portfolio">
-                <div className="row">
-                    <div className="twelve columns collapsed">
-                        <h1>Check Out Some of My Work.</h1>
-                        {/* <div
-                            id="portfolio-wrapper"
-                            className="bgrid-quarters s-bgrid-thirds cf"
-                        > */}
-                        {resumeData.portfolio &&
-                            resumeData.portfolio.map((item, i) => {
-                                return (
-                                    <div
-                                        key={i}
-                                        className="columns portfolio-item"
-                                    >
-                                        <div className="item-wrap">
-                                            <img
-                                                src={`${item.imgurl}`}
-                                                className="item-img"
-                                                alt={`${item.name}`}
-                                            />
-                                            <div className="overlay">
-                                                <div className="portfolio-item-meta">
-                                                    <h5>{item.name}</h5>
-                                                    <p>{item.description}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="portfolio-links">
-                                            <a
-                                                href={item.link}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <button className="link-btn">
-                                                    <h2>Demo</h2>
-                                                </button>
-                                            </a>
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id='portfolio'>
+        <div className='row'>
+          <div className='twelve columns collapsed'>
+            <h1>Check Out Some of My Work.</h1>
+            <div id='portfolio-wrapper' className='bgrid-thirds cf'>
+              {resumeData.portfolio &&
+                resumeData.portfolio.map((item, i) => {
+                  return (
+                    <div key={i} className='columns portfolio-item'>
+                      <div className='item-wrap'>
+                        <img
+                          src={`${item.imgurl}`}
+                          className='item-img'
+                          alt={`${item.name}`}
+                        />
+                        <div className='overlay'>
+                          <div className='portfolio-item-meta'>
+                            <h5>{item.name}</h5>
+                            <p>{item.description}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='portfolio-links'>
+                        <a
+                          href={item.link}
+                          target='_blank'
+                          rel='noopener noreferrer'
+                        >
+                          <button className='link-btn'>
+                            <h2>Demo</h2>
+                          </button>
+                        </a>
 
-                                            <a
-                                                href={item.github}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <button className="link-btn">
-                                                    <h2>Github</h2>
-                                                </button>
-                                            </a>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        {/* </div> */}
+                        <a
+                          href={item.github}
+                          target='_blank'
+                          rel='noopener noreferrer'
+                        >
+                          <button className='link-btn'>
+                            <h2>Github</h2>
+                          </button>
+                        </a>
+                      </div>
                     </div>
-                </div>
-            </section>
-        );
-    }
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,168 +1,176 @@
 let resumeData = {
-    name: "Reid Campbell",
-    role: "Full Stack Web Developer",
-    linkedinId: "ReidScottCampbell",
-    logo: "images/logo.png",
-    roleDescription:
-        " I like dabbling in various parts of web development and like to explore new technologies.",
-    socialLinks: [
-        {
-            name: "linkedin",
-            url: "https://www.linkedin.com/in/ReidScottCampbell/",
-            className: "fa fa-linkedin"
-        },
-        {
-            name: "github",
-            url: "http://github.com/ReidCampbell",
-            className: "fa fa-github"
-        }
-    ],
-    aboutme:
-        "I earned my Bachelor's degree in Marketing, Advertising and Branding from the University of Gloucestershire in 2017 and worked in Sales and Digital Marketing Performance positions since then. I've always been passionate about storytelling, photography, and branding, so right after I finished University, I started my own media production company, Beehive Basement Media. In 2019, after quite some time considering it, I finally took the time to learn to code and completed the Web Development Bootcamp at Le Wagon London. Today my goal is joining an ambitious team and building products people love. I'm eager to know more about new opportunities, feel free to reach out to me.",
-    address: "London",
-    email: "reidscottcampbell@gmail.com",
-    education: [
-        {
-            UniversityName: "Le Wagon",
-            specialization: "Full Stack Web Development",
-            MonthOfPassing: "June",
-            YearOfPassing: "2019",
-            Achievements: "Some Achievements"
-        },
-        {
-            UniversityName: "University of Gloucestershire",
-            specialization: "Marketing, Advertising and Branding",
-            MonthOfPassing: "May",
-            YearOfPassing: "2017",
-            Achievements: "2.1"
-        }
-    ],
-    work: [
-        {
-            CompanyName: "Le Wagon",
-            specialization: "Teacher / Teaching Assistant",
-            MonthOfStarting: "August",
-            YearOfStarting: "2020",
-            MonthOfLeaving: "Present",
-            YearOfLeaving: "",
-            Achievements: "Some Achievements"
-        },
-        {
-            CompanyName: "Adludio",
-            specialization: "Full Stack Developer",
-            MonthOfStarting: "November",
-            YearOfStarting: "2019",
-            MonthOfLeaving: "September",
-            YearOfLeaving: "2020",
-            Achievements: "Some Achievements"
-        },
-        {
-            CompanyName: "OMD",
-            specialization: "Biddable Performance Executive",
-            MonthOfStarting: "August",
-            YearOfStarting: "2018",
-            MonthOfLeaving: "April",
-            YearOfLeaving: "2019",
-            Achievements: "Some Achievements"
-        },
-        {
-            CompanyName: "JCDecaux",
-            specialization: "Strategic Solutions Sales Coordinator",
-            MonthOfStarting: "March",
-            YearOfStarting: "2018",
-            MonthOfLeaving: "August",
-            YearOfLeaving: "2018",
-            Achievements: "Some Achievements"
-        }
-    ],
-    skillsDescription: "Technologies",
-    skills: [
-        {
-            skillname: "HTML5",
-            logo: "devicon-html5-plain-wordmark colored"
-        },
-        {
-            skillname: "CSS",
-            logo: "devicon-css3-plain-wordmark colored"
-        },
-        {
-            skillname: "ES6",
-            logo: "devicon-javascript-plain colored"
-        },
-        {
-            skillname: "TypeScript",
-            logo: "devicon-typescript-plain colored"
-        },
-        {
-            skillname: "ReactJS",
-            logo: "devicon-react-original-wordmark colored"
-        },
-        {
-            skillname: "Redux",
-            logo: "devicon-redux- colored"
-        },
-        {
-            skillname: "Ruby",
-            logo: "devicon-ruby-plain-wordmark colored"
-        },
-        {
-            skillname: "Rails",
-            logo: "devicon-rails-plain-wordmark colored"
-        },
-        {
-            skillname: "MySQL",
-            logo: "devicon-mysql-plain-wordmark colored"
-        },
-        {
-            skillname: "Adobe Creative Suite",
-            logo: "devicon-photoshop-line colored"
-        },
-        {
-            skillname: "Git",
-            logo: "devicon-git-plain-wordmark colored"
-        }
-    ],
-    portfolio: [
-        {
-            name: "Gatsby Tours",
-            description: "Travel website created with GatsbyJS and Graphql",
-            imgurl: "images/portfolio/gatsby-tours.png",
-            link: "https://gatsbytours.netlify.com/",
-            github: "https://github.com/ReidCampbell/Gatsby-Tours"
-        },
-        {
-            name: "brunchee.",
-            description: "Web App created with Ruby-on-Rails",
-            imgurl: "images/portfolio/brunchee.gif",
-            link: "https://brunchee.herokuapp.com",
-            github: "https://github.com/ReidCampbell/brunchee"
-        }
-        // {
-        //     name: "project2",
-        //     description: "mobileapp",
-        //     imgurl: "images/portfolio/project.jpg"
-        // },
-        // {
-        //     name: "project3",
-        //     description: "mobileapp",
-        //     imgurl: "images/portfolio/project2.png"
-        // },
-        // {
-        //     name: "project4",
-        //     description: "mobileapp",
-        //     imgurl: "images/portfolio/phone.jpg"
-        // }
-    ]
-    // testimonials: [
-    //     {
-    //         description: "",
-    //         name: ""
-    //     },
-    //     {
-    //         description: "",
-    //         name: ""
-    //     }
-    // ]
+  name: 'Reid Campbell',
+  role: 'Full Stack Web Developer',
+  linkedinId: 'ReidScottCampbell',
+  logo: 'images/logo.png',
+  roleDescription:
+    ' I like dabbling in various parts of web development and like to explore new technologies.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ReidScottCampbell/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'http://github.com/ReidCampbell',
+      className: 'fa fa-github',
+    },
+  ],
+  aboutme:
+    "I earned my Bachelor's degree in Marketing, Advertising and Branding from the University of Gloucestershire in 2017 and worked in Sales and Digital Marketing Performance positions since then. I've always been passionate about storytelling, photography, and branding, so right after I finished University, I started my own media production company, Beehive Basement Media. In 2019, after quite some time considering it, I finally took the time to learn to code and completed the Web Development Bootcamp at Le Wagon London. Today my goal is joining an ambitious team and building products people love. I'm eager to know more about new opportunities, feel free to reach out to me.",
+  address: 'London',
+  email: 'reidscottcampbell@gmail.com',
+  education: [
+    {
+      UniversityName: 'Le Wagon',
+      specialization: 'Full Stack Web Development',
+      MonthOfPassing: 'June',
+      YearOfPassing: '2019',
+      Achievements: 'Some Achievements',
+    },
+    {
+      UniversityName: 'University of Gloucestershire',
+      specialization: 'Marketing, Advertising and Branding',
+      MonthOfPassing: 'May',
+      YearOfPassing: '2017',
+      Achievements: '2.1',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'Le Wagon',
+      specialization: 'Teacher / Teaching Assistant',
+      MonthOfStarting: 'August',
+      YearOfStarting: '2020',
+      MonthOfLeaving: 'Present',
+      YearOfLeaving: '',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'Adludio',
+      specialization: 'Full Stack Developer',
+      MonthOfStarting: 'November',
+      YearOfStarting: '2019',
+      MonthOfLeaving: 'September',
+      YearOfLeaving: '2020',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'OMD',
+      specialization: 'Biddable Performance Executive',
+      MonthOfStarting: 'August',
+      YearOfStarting: '2018',
+      MonthOfLeaving: 'April',
+      YearOfLeaving: '2019',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'JCDecaux',
+      specialization: 'Strategic Solutions Sales Coordinator',
+      MonthOfStarting: 'March',
+      YearOfStarting: '2018',
+      MonthOfLeaving: 'August',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  skillsDescription: 'Technologies',
+  skills: [
+    {
+      skillname: 'HTML5',
+      logo: 'devicon-html5-plain-wordmark colored',
+    },
+    {
+      skillname: 'CSS',
+      logo: 'devicon-css3-plain-wordmark colored',
+    },
+    {
+      skillname: 'ES6',
+      logo: 'devicon-javascript-plain colored',
+    },
+    {
+      skillname: 'TypeScript',
+      logo: 'devicon-typescript-plain colored',
+    },
+    {
+      skillname: 'ReactJS',
+      logo: 'devicon-react-original-wordmark colored',
+    },
+    {
+      skillname: 'Redux',
+      logo: 'devicon-redux- colored',
+    },
+    {
+      skillname: 'Ruby',
+      logo: 'devicon-ruby-plain-wordmark colored',
+    },
+    {
+      skillname: 'Rails',
+      logo: 'devicon-rails-plain-wordmark colored',
+    },
+    {
+      skillname: 'MySQL',
+      logo: 'devicon-mysql-plain-wordmark colored',
+    },
+    {
+      skillname: 'Adobe Creative Suite',
+      logo: 'devicon-photoshop-line colored',
+    },
+    {
+      skillname: 'Git',
+      logo: 'devicon-git-plain-wordmark colored',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'Reiddit',
+      description:
+        'Reddit clone created with React, Node, Typescript, Graphql with Apollo, and Postgres',
+      imgurl: 'images/portfolio/reiddit.png',
+      link: 'https://reiddit.reidcampbell.xyz/',
+      github: 'https://github.com/ReidCampbell/Reiddit-Client',
+    },
+    {
+      name: 'Gatsby Tours',
+      description: 'Travel website created with GatsbyJS and Graphql',
+      imgurl: 'images/portfolio/gatsby-tours.png',
+      link: 'https://gatsbytours.netlify.com/',
+      github: 'https://github.com/ReidCampbell/Gatsby-Tours',
+    },
+    {
+      name: 'brunchee.',
+      description: 'Web App created with Ruby-on-Rails',
+      imgurl: 'images/portfolio/brunchee.gif',
+      link: 'https://brunchee.herokuapp.com',
+      github: 'https://github.com/ReidCampbell/brunchee',
+    },
+    // {
+    //     name: "project2",
+    //     description: "mobileapp",
+    //     imgurl: "images/portfolio/project.jpg"
+    // },
+    // {
+    //     name: "project3",
+    //     description: "mobileapp",
+    //     imgurl: "images/portfolio/project2.png"
+    // },
+    // {
+    //     name: "project4",
+    //     description: "mobileapp",
+    //     imgurl: "images/portfolio/phone.jpg"
+    // }
+  ],
+  // testimonials: [
+  //     {
+  //         description: "",
+  //         name: ""
+  //     },
+  //     {
+  //         description: "",
+  //         name: ""
+  //     }
+  // ]
 };
 
 export default resumeData;
